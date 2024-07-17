df25e9525d5883665a17d51cd6140c3c24a0959a
Max Gagner <m.gagner@hotmail.se>
2024-07-17 17:28:59 +0200

    initial setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from '../app/index/index.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: IndexComponent}
+];
