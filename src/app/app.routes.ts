7e069f2b3c4fc444900af162c8e8acb193cbd9ba
Max Gagner <m.gagner@hotmail.se>
2024-07-17 20:13:58 +0200

    menu completed with new component (non resizable)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component'
+import { SchemaComponent } from './schema/schema.component';
 
 export const routes: Routes = [
-    {path: "", component: CoursesComponent}
+    {path: "", component: CoursesComponent},
+    {path: "schema", component: SchemaComponent}
 ];
---
+++ src/app/schema/schema.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schema',
+  standalone: true,
+  imports: [],
+  templateUrl: './schema.component.html',
+  styleUrl: './schema.component.css'
+})
+export class SchemaComponent {
+
+}
