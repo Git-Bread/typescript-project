e02d628aec2d70d7764147677941d249ade3c609
Max Gagner <m.gagner@hotmail.se>
2024-07-24 17:33:19 +0200

    added html and typescript to schema, builds on the same functions as the index page. Aswell as a small typo fix in the content box.

--- src/app/schema/schema.component.ts
+++ src/app/schema/schema.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { LocalStorageBossService } from '../../services/local-storage-boss.service';
+import { Obj } from '../../assets/Obj';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SearcherService } from '../../services/searcher.service';
+import { SorterService } from '../../services/sorter.service';
 
 @Component({
   selector: 'app-schema',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './schema.component.html',
   styleUrl: './schema.component.css'
 })
 export class SchemaComponent {
+  savedCourses: Obj[] = [];
+  contentArchive: Obj[] = [];
 
+  //form setup
+  formHandle = new FormGroup({
+    input: new FormControl("", Validators.required)
+  })
+
+  constructor(private localStorageBoss : LocalStorageBossService, private searcher : SearcherService, private sorter : SorterService) {}
+
+
+  ngOnInit() {
+      this.savedCourses = this.localStorageBoss.sync(); 
+  }
+
+  search() {
+    this.savedCourses = this.contentArchive;
+    this.savedCourses = this.searcher.searchSort(this.formHandle.value.input!,  this.contentArchive, 0, false, this.contentArchive);
+  }
+
+  remove(obj: Obj) {
+    this.localStorageBoss.remove(obj);
+  }
+
+  sort(num: number){
+    this.savedCourses = this.sorter.simpleSort(num, this.savedCourses);
+  }
+
+  //removes from localhost
+  localRemove(obj : Obj) {
+    this.localStorageBoss.remove(obj);
+    this.savedCourses = this.localStorageBoss.sync(); 
+  }
 }
